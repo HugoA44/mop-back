416659dc771260b2398aa45c75138036b195539e
hugo@122 <hugo@scorenco.com>
2022-04-20 15:17:35 +0200

    log

--- utils/getTravel.js
+++ utils/getTravel.js
@@ -8,51 +8,53 @@ const getTravel = async (_placetype, _lat, _long, _ray) => {
 
   let parcours = [];
 
-  const mapping = await Promise.all(
-    placetype.map(async (type, index) => {
-      const url = `https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(${type}(${
-        lat - ray
-      },${long - ray},${lat + ray},${long + ray}););out;>;out skel qt;`;
+  const mapping = async () => {
+    Promise.all(
+      placetype.map(async (type, index) => {
+        const url = `https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];(${type}(${
+          lat - ray
+        },${long - ray},${lat + ray},${long + ray}););out;>;out skel qt;`;
 
-      await axios({
-        method: "get",
-        url,
-      })
-        .then(async function (response) {
-          const places = response.data.elements;
-
-          const selectedPlace =
-            places[Math.floor(Math.random() * response.data.elements.length)];
-
-          console.log("index", index);
-          console.log("parcours avant travel", parcours);
-
-          const travelUrl = `https://api.external.citymapper.com/api/1/traveltimes?start=${
-            index > 0 ? parcours[index - 1][1].lat : lat
-          },${index > 0 ? parcours[index - 1][1].lon : long}&end=${Number(
-            selectedPlace.lat
-          )},${Number(selectedPlace.lon)}`;
-
-          await axios({
-            method: "get",
-            url: travelUrl,
-            headers: {
-              "Citymapper-Partner-Key": process.env.CITYMAPPER_API,
-            },
-          })
-            .then(function (response) {
-              parcours.push([response.data, selectedPlace]);
-              console.log("parcours après travel", parcours);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
+        await axios({
+          method: "get",
+          url,
         })
-        .catch(function (error) {
-          console.log(error);
-        });
-    })
-  );
+          .then(async function (response) {
+            const places = response.data.elements;
+
+            const selectedPlace =
+              places[Math.floor(Math.random() * response.data.elements.length)];
+
+            console.log("index", index);
+            console.log("parcours avant travel", parcours);
+
+            const travelUrl = `https://api.external.citymapper.com/api/1/traveltimes?start=${
+              index > 0 ? parcours[index - 1][1].lat : lat
+            },${index > 0 ? parcours[index - 1][1].lon : long}&end=${Number(
+              selectedPlace.lat
+            )},${Number(selectedPlace.lon)}`;
+
+            await axios({
+              method: "get",
+              url: travelUrl,
+              headers: {
+                "Citymapper-Partner-Key": process.env.CITYMAPPER_API,
+              },
+            })
+              .then(function (response) {
+                parcours.push([response.data, selectedPlace]);
+                console.log("parcours après travel", parcours);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      })
+    );
+  };
 
   return parcours;
 };
